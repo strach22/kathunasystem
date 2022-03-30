6106ee376657fba787d6688d173358005c7e66a7
SebasDiUks <sebastian.lovato@outlook.com>
2022-03-30 17:56:19 -0500

    Primer Commit General - Diuks

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import { AddCategory } from "./components/AddCategory";
-import { AddGif } from "./components/AddGif";
 import "./indexSingUp.css";
 
 function Cover() {
-  const [categories, setCategories] = useState(["Marvel", "DC", "Anime"]);
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <h2>LISTADO DE GIFS</h2>
-      <AddCategory setCategories={setCategories} />
-      <hr />
-      <ol>
-        {categories.map((category) => (
-          <AddGif key={category} category={category} />
-        ))}
-      </ol>
     </DashboardLayout>
   );
 }
