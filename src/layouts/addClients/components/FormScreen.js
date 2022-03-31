b381f5daf2423bcc54fac8d0f9b1e2dd292e8c9c
SebasDiUks <sebastian.lovato@outlook.com>
2022-03-31 18:39:29 -0500

    31/03 Tercer Commit

---
+++ src/layouts/addClients/components/FormScreen.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import Input from "../elements/Input";
+import * as Helpers from "../helpers/Helpers";
+import RadioG from "../elements/RadioG";
+import SelectG from "../elements/SelectG";
+import DatePickerH from "../elements/DatePickerH";
+import CheckboxB from "../elements/CheckboxB";
+import useForm from "../hooks/useForm";
+
+const initialValues = {
+  id: 0,
+  name: "",
+  lastname: "",
+  identification: "",
+  mobile: "",
+  email: "",
+  direction: "",
+  tariff: "",
+  civil: "",
+  birthDate: new Date(),
+  agree: false,
+};
+
+export default function FormScreen() {
+  const { values, handleInputChange } = useForm(initialValues);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const {
+    firstname,
+    lastname,
+    identification,
+    mobile,
+    email,
+    direction,
+    tariff,
+    civil,
+    birthDate,
+    agree,
+  } = values;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container>
+        <Grid item xs={6}>
+          <Input label="Nombres" name="names" value={firstname} onChange={handleInputChange} />
+          <Input label="Apellidos" name="lastname" value={lastname} onChange={handleInputChange} />
+          <Input
+            label="Cédula de Indentidad"
+            name="identification"
+            value={identification}
+            onChange={handleInputChange}
+          />
+          <Input
+            label="Número de teléfono"
+            name="mobile"
+            value={mobile}
+            onChange={handleInputChange}
+          />
+          <Input label="Email" name="email" value={email} onChange={handleInputChange} />
+          <Input
+            label="Dirección"
+            name="direction"
+            value={direction}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <RadioG
+            name="tariff"
+            label="Tarifa"
+            value={tariff}
+            onChange={handleInputChange}
+            items={Helpers.tariffItems()}
+          />
+          <SelectG
+            name="civil"
+            label="Estado Civil"
+            value={civil}
+            onChange={handleInputChange}
+            options={Helpers.getCivilStatus()}
+          />
+
+          <DatePickerH
+            name="birthDate"
+            label="Fecha de Nacimiento"
+            value={birthDate}
+            onChange={handleInputChange}
+          />
+          <CheckboxB
+            name="agree"
+            label="Desea Añadir Cliente"
+            value={agree}
+            onChange={handleInputChange}
+          />
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
--- src/layouts/addClients/components/PrincipalScreen.js
+++ src/layouts/addClients/components/PrincipalScreen.js
@@ -2,48 +2,9 @@ import React from "react";
 import { Card, Grid } from "@mui/material";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import useForm from "../hooks/useForm";
-import Input from "../elements/Input";
-import * as Helpers from "../helpers/Helpers";
-import RadioG from "../elements/RadioG";
-import SelectG from "../elements/SelectG";
-import DatePickerH from "../elements/DatePickerH";
-import CheckboxB from "../elements/CheckboxB";
-
-const initialValues = {
-  id: 0,
-  name: "",
-  lastname: "",
-  identification: "",
-  mobile: "",
-  email: "",
-  direction: "",
-  tariff: "",
-  civil: "",
-  birthDate: new Date(),
-  agree: false,
-};
+import FormScreen from "./FormScreen";
 
 export default function PrincipalScreen() {
-  const { values, handleInputChange } = useForm(initialValues);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const {
-    firstname,
-    lastname,
-    identification,
-    mobile,
-    email,
-    direction,
-    tariff,
-    civil,
-    birthDate,
-    agree,
-  } = values;
-
   return (
     <MDBox pt={6} pb={3}>
       <Grid container spacing={6}>
@@ -64,72 +25,7 @@ export default function PrincipalScreen() {
               </MDTypography>
             </MDBox>
             <MDBox pt={3}>
-              <form onSubmit={handleSubmit}>
-                <Grid container>
-                  <Grid item xs={6}>
-                    <Input
-                      label="Nombres"
-                      name="names"
-                      value={firstname}
-                      onChange={handleInputChange}
-                    />
-                    <Input
-                      label="Apellidos"
-                      name="lastname"
-                      value={lastname}
-                      onChange={handleInputChange}
-                    />
-                    <Input
-                      label="Cédula de Indentidad"
-                      name="identification"
-                      value={identification}
-                      onChange={handleInputChange}
-                    />
-                    <Input
-                      label="Número de teléfono"
-                      name="mobile"
-                      value={mobile}
-                      onChange={handleInputChange}
-                    />
-                    <Input label="Email" name="email" value={email} onChange={handleInputChange} />
-                    <Input
-                      label="Dirección"
-                      name="direction"
-                      value={direction}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <RadioG
-                      name="tariff"
-                      label="Tarifa"
-                      value={tariff}
-                      onChange={handleInputChange}
-                      items={Helpers.tariffItems()}
-                    />
-                    <SelectG
-                      name="civil"
-                      label="Estado Civil"
-                      value={civil}
-                      onChange={handleInputChange}
-                      options={Helpers.getCivilStatus()}
-                    />
-
-                    <DatePickerH
-                      name="birthDate"
-                      label="Fecha de Nacimiento"
-                      value={birthDate}
-                      onChange={handleInputChange}
-                    />
-                    <CheckboxB
-                      name="agree"
-                      label="Desea Añadir Cliente"
-                      value={agree}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                </Grid>
-              </form>
+              <FormScreen />
             </MDBox>
           </Card>
         </Grid>
