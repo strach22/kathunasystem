36d77387e6b87fff2d81c1cd6b525c9459945a79
Isaac <isaacandrespozo@gmail.com>
2022-04-07 09:39:08 -0500

    Add retiro form

--- src/layouts/depositos/depositoForm/index.js
+++ src/layouts/depositos/depositoForm/index.js
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 // @mui material components
 import Grid from "@mui/material/Grid";
 import MDButton from "components/MDButton";
@@ -16,6 +16,12 @@ import clients from "../../../data/clients.json";
 
 function Deposito() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const depositar = () => {
+    clients[id - 1].saldoAhorros += 10;
+    navigate("/inicio");
+  };
   return (
     <DashboardLayout>
       <DashboardNavbar />
@@ -45,8 +51,8 @@ function Deposito() {
                 REGRESAR
               </MDButton>
             </Link>
-            <MDButton color="info" sx={{ marginLeft: 2 }}>
-              EDITAR
+            <MDButton color="info" onClick={depositar} sx={{ marginLeft: 2 }}>
+              DEPOSITAR
             </MDButton>
             <MDButton color="error" sx={{ marginLeft: 2 }}>
               ELIMINAR
