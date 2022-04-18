65e5c7b64eb6dd347cc5fa0215437da32961d977
SebasDiUks <sebastian.lovato@outlook.com>
2022-04-18 17:19:01 -0500

    React-Excel-Render and xlsx

---
+++ src/layouts/cargaClientes/components/PrincipalScreen.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card, Grid } from "@mui/material";
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import UploadClientsScreen from "./UploadClientsScreen";
+
+export default function PrincipalScreen() {
+  return (
+    <MDBox pt={6} pb={3} mx={15}>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Card>
+            <MDBox
+              mx={2}
+              mt={-3}
+              py={3}
+              px={2}
+              variant="gradient"
+              bgColor="info"
+              borderRadius="lg"
+              coloredShadow="info"
+            >
+              <MDTypography variant="h5" color="white">
+                Carga de Clientes
+              </MDTypography>
+            </MDBox>
+            <MDBox pt={3} sx={{ margin: "30px", padding: "24px" }}>
+              <UploadClientsScreen />
+            </MDBox>
+          </Card>
+        </Grid>
+      </Grid>
+    </MDBox>
+  );
+}
---
+++ src/layouts/cargaClientes/components/UploadClientsScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UploadClientsScreen() {
+  return <div>UploadClientsScreen</div>;
+}
