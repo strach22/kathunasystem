0daa89dd1d5064a51af53adea9b9272e7a8de410
SebasDiUks <sebastian.lovato@outlook.com>
2022-04-21 09:37:36 -0500

    Descarga del formato de clientes

---
+++ src/layouts/cargaClientes/components/ExcelExport.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MDButton from "components/MDButton";
+import { Workbook } from "react-excel-workbook";
+
+import ArticleIcon from "@mui/icons-material/Article";
+
+export default function ExcelExport({ filename = "", worksheets = [] }) {
+  return (
+    <div>
+      <Workbook
+        filename={filename}
+        element={
+          <MDButton color="success">
+            <ArticleIcon color="dark" fontSize="large" sx={{ marginRight: 1 }} />
+            Descargar
+          </MDButton>
+        }
+      >
+        {worksheets.map(({ name, columns, data }) => (
+          <Workbook.Sheet name={name} data={data}>
+            {columns.map(({ label, value }) => (
+              <Workbook.Column label={label} value={value} />
+            ))}
+          </Workbook.Sheet>
+        ))}
+      </Workbook>
+    </div>
+  );
+}
+
+ExcelExport.propTypes = {
+  filename: PropTypes.string.isRequired,
+  worksheets: PropTypes.string.isRequired,
+};
--- src/layouts/cargaClientes/components/UploadClientsScreen.js
+++ src/layouts/cargaClientes/components/UploadClientsScreen.js
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import ExcelExport from "./ExcelExport";
+import ExcelImport from "./ExcelImport";
 
 export default function UploadClientsScreen() {
-  return <div>UploadClientsScreen</div>;
+  const [data, setData] = useState([]);
+
+  console.log(data);
+
+  const worksheets = [
+    {
+      name: "Requisitos",
+      columns: [
+        { label: "Code", value: "code" },
+        { label: "Fecha_Apertura", value: "fechaApertura" },
+        { label: "Estado_Civil", value: "estadoCivil" },
+        { label: "Nombres", value: "nombres" },
+        { label: "Apellidos", value: "apellidos" },
+        { label: "Número_Cédula", value: "numeroCedula" },
+        { label: "Dirección", value: "direccion" },
+        { label: "Parentesco", value: "parentesco" },
+        { label: "Nombre_Parentesco", value: "nombreParentesco" },
+        { label: "Cédula_Parentesco", value: "cedulaParentesco" },
+        { label: "Teléfono_Parentesco", value: "telefonoParentesco" },
+        { label: "Nombre_Conyugue", value: "nombreConyugue" },
+        { label: "Cédula_Conyugue", value: "cedulaConyugue" },
+        { label: "Teléfono_Conyugue", value: "telefonoConyugue" },
+        { label: "Teléfono", value: "telefono" },
+        { label: "Tarifa", value: "tarifa" },
+      ],
+      data: [
+        {
+          code: "0006",
+          fechaApertura: "2020-01-17",
+          estadoCivil: "NULL",
+          nombres: "NORAYMA KATERINE",
+          apellidos: "CUASCOTA GUASGUA",
+          numeroCedula: "1724530876",
+          direccion: "san pablito de agualongo",
+          parentesco: "MADRE",
+          nombreParentesco: "MARIA RUFINA GUASGUA",
+          cedulaParentesco: "1711709962",
+          telefonoParentesco: "NULL",
+          nombreConyugue: "NULL",
+          cedulaConyugue: "NULL",
+          telefonoConyugue: "NULL",
+          telefono: "987901520",
+          tarifa: "SOCIO",
+        },
+        {
+          code: "0007",
+          fechaApertura: "2020-01-17",
+          estadoCivil: "NULL",
+          nombres: "NORAYMA KATERINE",
+          apellidos: "CUASCOTA GUASGUA",
+          numeroCedula: "1724530876",
+          direccion: "san pablito de agualongo",
+          parentesco: "MADRE",
+          nombreParentesco: "MARIA RUFINA GUASGUA",
+          cedulaParentesco: "1711709962",
+          telefonoParentesco: "NULL",
+          nombreConyugue: "NULL",
+          cedulaConyugue: "NULL",
+          telefonoConyugue: "NULL",
+          telefono: "987901520",
+          tarifa: "SOCIO",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <ExcelExport filename="lista-de-clientes.xlsx" worksheets={worksheets} />
+      <ExcelImport uploadHandler={setData} />
+    </div>
+  );
 }
