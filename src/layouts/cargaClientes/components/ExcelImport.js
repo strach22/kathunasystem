db32ec13e8f30d96324499356aeef72ef6102e88
SebasDiUks <sebastian.lovato@outlook.com>
2022-04-21 10:05:03 -0500

    Primer versión de la Importación desde Excel

--- src/layouts/cargaClientes/components/ExcelImport.js
+++ src/layouts/cargaClientes/components/ExcelImport.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import MDButton from "components/MDButton";
+import MDTypography from "components/MDTypography";
+import { ExcelRenderer, OutTable } from "react-excel-renderer";
 
-export default function ExcelImport() {
-  return <div>ExcelImport</div>;
+export default function ExcelImport(uploadHandler) {
+  const [state, setState] = useState({ cols: [], rows: [] });
+
+  const uploadFile = (e) => {
+    const fileObj = e.target.files[0];
+    ExcelRenderer(fileObj, (err, resp) => {
+      if (!err) {
+        const { cols, rows } = resp;
+        setState(
+          {
+            cols,
+            rows,
+          },
+          () => {
+            const data = [...rows];
+            data.shift();
+            uploadHandler(data);
+          }
+        );
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <MDTypography>Subir Archivo</MDTypography>
+        <input type="file" onChange={uploadFile} />
+        <MDButton color="success">+</MDButton>
+      </div>
+      <div>
+        <OutTable data={state.rows} columns={state.cols} />
+      </div>
+    </div>
+  );
 }
