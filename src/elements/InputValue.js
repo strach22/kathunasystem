b4febdd350c6e6750cdbc2656943a8e03c10fd08
SebasDiUks <sebastian.lovato@outlook.com>
2022-04-29 17:36:09 -0500

    DepositScreen casi terminado

--- src/elements/InputValue.js
+++ src/elements/InputValue.js
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import { InputAdornment, OutlinedInput } from "@mui/material";
+import { FormControl, FormHelperText, InputAdornment, OutlinedInput } from "@mui/material";
 // import PropTypes from "prop-types";
 
 export default function InputValue(props) {
-  // const { name, error, value, onChange } = props;
-  const { name, value, onChange } = props;
+  const { name, value, onChange, error } = props;
+
   return (
-    <OutlinedInput
-      id="outlined-adornment-amount"
-      name={name}
-      value={value}
-      onChange={onChange}
-      startAdornment={<InputAdornment position="start">$</InputAdornment>}
-      // {...(error && { error: true, helperText: error })}
-    />
+    <FormControl>
+      <OutlinedInput
+        id="outlined-adornment-amount"
+        name={name}
+        value={value}
+        onChange={onChange}
+        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+        {...(error && { error: true })}
+      />
+      {error && <FormHelperText error>{error}</FormHelperText>}
+    </FormControl>
   );
 }
 
--- src/layouts/depositos/depositoForm/components/DepositScreen.js
+++ src/layouts/depositos/depositoForm/components/DepositScreen.js
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable no-use-before-define */
+import React, { useContext } from "react";
 import { Grid } from "@mui/material";
 import MDTypography from "components/MDTypography";
 import DatePickerH from "elements/DatePickerH";
@@ -7,42 +8,89 @@ import InputValue from "elements/InputValue";
 import TextArea from "elements/TextArea";
 import ButtonOk from "elements/ButtonOk";
 import { Link } from "react-router-dom";
+import ClientsContext from "context/Clients/ClientsContext";
 import Form from "../helpers/Form";
 
 export default function DepositScreen() {
-  const { values, handleInputChange, resetForm } = useForm({
-    depositDate: new Date(),
-    actualBalance: 0,
-  });
+  const errorValues = {
+    actualBalance: "",
+  };
+
+  // eslint-disable-next-line consistent-return
+  const validate = (fieldValues = values) => {
+    const tempo = { ...errors };
+
+    if ("actualBalance" in fieldValues) {
+      tempo.actualBalance = /^[0-9]+$/.test(fieldValues.actualBalance)
+        ? ""
+        : "Llenar el campo. No se permite letras";
+    }
+    setErrors({
+      ...tempo,
+    });
+    if (fieldValues === values) return Object.values(tempo).every((x) => x === "");
+  };
+
+  const { values, errors, setErrors, handleInputChange, resetForm } = useForm(
+    {
+      transactionDate: new Date(),
+      actualBalance: 0,
+    },
+    true,
+    validate,
+    errorValues
+  );
+
+  const { clients } = useContext(ClientsContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (validate()) {
+      const newTransactionDate = values.transactionDate.toISOString().split("T")[0];
+      values.transactionDate = newTransactionDate;
+
+      console.log(clients);
+
+      // if (values.id === "0") values.id = String(parseInt(clients[clients.length - 1].id, 10) + 1);
+      // addClient(values);
+      // resetForm();
+      // resetClientInfo();
+      // navigate("/clientes");
+    }
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Grid container>
         <Grid item xs={4}>
-          <MDTypography className="Subtitles">Fecha de la transacción:</MDTypography>
+          <MDTypography className="Subtitles" variant="h5">
+            Fecha de la transacción:
+          </MDTypography>
         </Grid>
         <Grid item xs={8}>
           <DatePickerH
-            name="depositDate"
+            name="transactionDate"
             label="Fecha de transacción"
-            value={values.depositDate}
+            value={values.transactionDate}
             onChange={handleInputChange}
           />
         </Grid>
         <Grid item xs={5}>
-          <MDTypography className="Subtitles">Valor a depositar:</MDTypography>
+          <MDTypography className="Subtitles" variant="h5">
+            Valor a depositar:
+          </MDTypography>
           <InputValue
             name="actualBalance"
             value={values.actualBalance}
             onChange={handleInputChange}
+            error={errors.actualBalance}
           />
         </Grid>
         <Grid xs={7}>
-          <MDTypography className="Subtitles">Observaciones:</MDTypography>
+          <MDTypography className="Subtitles" variant="h5">
+            Observaciones:
+          </MDTypography>
           <TextArea
             minRows={3}
             maxRows={4}
