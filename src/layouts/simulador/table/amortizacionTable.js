64eb078ff5c8563639aa3767282444c4a254af2d
Isaac <isaacandrespozo@gmail.com>
2022-05-11 22:18:21 -0500

    tabla de amortización

--- src/layouts/simulador/table/amortizacionTable.js
+++ src/layouts/simulador/table/amortizacionTable.js
@@ -1,27 +1,30 @@
 export default function data({ loanValue, periods, interes }) {
-  const n = "1";
   const periodicFee = loanValue * (interes / (1 - (interes + 1) ** -periods));
   const desgravamen = +(0.01 * loanValue) / periods;
   const periodicFeeDesgravamen = periodicFee + desgravamen;
-  const periodInteres = loanValue * interes;
-  const rows = loanValue
-    ? [
-        {
-          cuota: n,
-          interesPeriodo: periodInteres.toFixed(2),
-          capitalAmortizado: "63.46",
-          desgravamen: desgravamen.toFixed(2),
-          valorCuota: periodicFeeDesgravamen.toFixed(2),
-          saldo: "2936.54",
-        },
-      ]
-    : [];
+  let periodInteres = loanValue * interes;
+  let amortizedCapital = periodicFee - periodInteres;
+  let residue = loanValue - amortizedCapital;
+  const rows = [];
+  for (let i = 1; i < periods + 1; i += 1) {
+    rows.push({
+      cuota: i,
+      interesPeriodo: periodInteres.toFixed(2),
+      capitalAmortizado: amortizedCapital.toFixed(2),
+      desgravamen: desgravamen.toFixed(2),
+      valorCuota: periodicFeeDesgravamen.toFixed(2),
+      saldo: residue.toFixed(2),
+    });
+    periodInteres = residue * interes;
+    amortizedCapital = periodicFee - periodInteres;
+    residue -= amortizedCapital;
+  }
   return {
     columns: [
-      { Header: "cuotas", accessor: "cuota", align: "left" },
-      { Header: "interes periodo", accessor: "interesPeriodo", align: "left" },
-      { Header: "capital amortizado", accessor: "capitalAmortizado", align: "left" },
-      { Header: "desrgavamen", accessor: "desgravamen", align: "left" },
+      { Header: "cuotas", accessor: "cuota", align: "center" },
+      { Header: "interes periodo", accessor: "interesPeriodo", align: "center" },
+      { Header: "capital amortizado", accessor: "capitalAmortizado", align: "center" },
+      { Header: "desrgavamen", accessor: "desgravamen", align: "center" },
       { Header: "valor cuota", accessor: "valorCuota", align: "center" },
       { Header: "saldo", accessor: "saldo", align: "center" },
     ],
