2bce954d31036ea7ddf1210b4b0e25e79f3a4ab5
Isaac <isaacandrespozo@gmail.com>
2022-05-17 18:23:11 -0500

    unnecesary div

--- src/layouts/transaccionesAhorros/helpers/AccountStatusScreen.js
+++ src/layouts/transaccionesAhorros/helpers/AccountStatusScreen.js
@@ -4,15 +4,13 @@ import historial from "../table/tableHistory";
 export default function AccountStatusScreen() {
   const { columns, rows } = historial();
   return (
-    <div>
-      <DataTable
-        table={{ columns, rows }}
-        isSorted
-        showTotalEntries={false}
-        noEndBorder
-        entriesPerPage={false}
-        defaultEntries={5}
-      />
-    </div>
+    <DataTable
+      table={{ columns, rows }}
+      isSorted
+      showTotalEntries={false}
+      noEndBorder
+      entriesPerPage={false}
+      defaultEntries={5}
+    />
   );
 }
