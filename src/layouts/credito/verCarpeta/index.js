71006682fc9cffe97b29dc7fed0575bf9fa386b6
Isaac <isaacandrespozo@gmail.com>
2022-05-19 11:04:10 -0500

    folder info

--- src/layouts/credito/verCarpeta/index.js
+++ src/layouts/credito/verCarpeta/index.js
@@ -1,13 +1,23 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 
+import ClientsContext from "context/Clients/ClientsContext";
+
 function verCarpeta() {
+  const { clients } = useContext(ClientsContext);
+  const { id } = useParams();
+
+  const folderInfo = clients.filter((client) => client.credits.includes(id));
+  console.log(folderInfo);
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      Carpeta Info
+      {folderInfo.loanValue}
       <Footer />
     </DashboardLayout>
   );
