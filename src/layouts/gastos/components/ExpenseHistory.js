4c5955e23b99b558a0781dcdef347bb209638926
SebasDiUks <sebastian.lovato@outlook.com>
2022-05-25 20:17:48 -0500

    PDF Hisotrial de Gastos

--- src/layouts/gastos/components/ExpenseHistory.js
+++ src/layouts/gastos/components/ExpenseHistory.js
@@ -1,15 +1,20 @@
+import { Grid } from "@mui/material";
 import DataTable from "examples/Tables/DataTable";
+import PaymentExpensesHistory from "../download/PaymentExpensesHistory";
 import ExpenseTable from "../table/expensesTable";
 
 export default function ExpenseHistory() {
   const { columns, rows } = ExpenseTable();
 
   return (
-    <DataTable
-      table={{ columns, rows }}
-      showTotalEntries={false}
-      noEndBorder
-      entriesPerPage={false}
-    />
+    <Grid container>
+      <PaymentExpensesHistory rows={rows} />
+      <DataTable
+        table={{ columns, rows }}
+        showTotalEntries={false}
+        noEndBorder
+        entriesPerPage={false}
+      />
+    </Grid>
   );
 }
--- src/layouts/gastos/download/PaymentExpensesHistory.js
+++ src/layouts/gastos/download/PaymentExpensesHistory.js
@@ -1,25 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { Grid } from "@mui/material";
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import MDButton from "components/MDButton";
-import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
-import { numbToLetters } from "../helpers/numbToLetters";
-import zfill from "../helpers/zfill";
 
-export default function ProofPaymentExpenses({ info }) {
-  const { id, expenseDate, expenseValue, observation } = info;
-
-  const lettersExpenseValue = numbToLetters(expenseValue, {
-    plural: "dólares estadounidenses",
-    singular: "dólar estadounidense",
-    centPlural: "centavos",
-    centSingular: "centavo",
-  });
+// eslint-disable-next-line react/prop-types
+export default function PaymentExpensesHistory({ rows }) {
+  // const { id, expenseDate, expenseValue, observation } = rows;
+  console.log(rows);
 
   const handleGeneratedPDF = () => {
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
-    const proofExpenses = {
+    const expensisHistory = {
       pageMargins: [40, 40, 40, 80],
       content: [
         {
@@ -35,90 +27,12 @@ export default function ProofPaymentExpenses({ info }) {
                 margin: [0, 0, 0, 3],
               },
               {
-                text: "''SEMBRANDO EL FUTURO IMPULSANDO AL DESARROLLO''",
+                text: "HISTORIAL DE GASTOS",
                 color: "#333333",
                 width: "*",
                 fontSize: 15,
                 bold: true,
                 alignment: "center",
-                margin: [0, 0, 0, 20],
-              },
-              {
-                stack: [
-                  {
-                    columns: [
-                      {
-                        text: "Representante Legal",
-                        color: "#aaaaab",
-                        bold: true,
-                        fontSize: 12,
-                        alignment: "left",
-                        margin: [0, 0, 0, 3],
-                      },
-                      {
-                        text: "Recibo No.",
-                        color: "#aaaaab",
-                        bold: true,
-                        width: "*",
-                        fontSize: 12,
-                        alignment: "right",
-                      },
-                      {
-                        text: zfill(parseInt(id, 10), 5),
-                        bold: true,
-                        color: "#333333",
-                        fontSize: 11,
-                        alignment: "right",
-                        width: 100,
-                      },
-                    ],
-                  },
-                  {
-                    text: "Nombre del Representante Legal",
-                    bold: true,
-                    color: "#333333",
-                    fontSize: 11,
-                    alignment: "left",
-                    margin: [10, 0, 0, 3],
-                  },
-                  {
-                    columns: [
-                      {
-                        text: "Dirección",
-                        color: "#aaaaab",
-                        bold: true,
-                        fontSize: 12,
-                        alignment: "left",
-                        margin: [0, 0, 0, 3],
-                      },
-                      {
-                        text: "Fecha de Emisión",
-                        color: "#aaaaab",
-                        bold: true,
-                        width: "*",
-                        fontSize: 12,
-                        alignment: "right",
-                      },
-                      {
-                        text: expenseDate,
-                        bold: true,
-                        color: "#333333",
-                        fontSize: 11,
-                        alignment: "right",
-                        width: 100,
-                      },
-                    ],
-                  },
-                  {
-                    text: "SAN PABLITO",
-                    color: "#333333",
-                    width: "*",
-                    fontSize: 11,
-                    bold: true,
-                    alignment: "left",
-                    margin: [10, 0, 0, 3],
-                  },
-                ],
               },
             ],
           ],
@@ -126,23 +40,6 @@ export default function ProofPaymentExpenses({ info }) {
         {
           text: "_________________________________________________________________",
           style: "border",
-        },
-        {
-          width: "100%",
-          alignment: "center",
-          text: "COMPROBANTE DE EGRESOS",
-          bold: true,
-          margin: [0, 10, 0, 2],
-          fontSize: 15,
-        },
-        {
-          width: "100%",
-          alignment: "center",
-          text: "GASTOS DE CAJA",
-          bold: true,
-          color: "red",
-          margin: [0, 0, 0, 20],
-          fontSize: 13,
         },
         {
           layout: {
@@ -175,31 +72,46 @@ export default function ProofPaymentExpenses({ info }) {
             body: [
               [
                 {
-                  text: "DETALLE",
+                  text: "CODIGO",
                   fillColor: "#eaf2f5",
                   border: [false, true, false, true],
                   margin: [0, 5, 0, 5],
                   textTransform: "uppercase",
                 },
                 {
-                  text: "VALOR",
+                  text: "FECHA",
                   border: [false, true, false, true],
                   alignment: "right",
                   fillColor: "#eaf2f5",
                   margin: [0, 5, 0, 5],
                   textTransform: "uppercase",
                 },
+                {
+                  text: "RAZÓN",
+                  border: [false, true, false, true],
+                  alignment: "right",
+                  fillColor: "#eaf2f5",
+                  margin: [0, 5, 0, 5],
+                  textTransform: "uppercase",
+                },
+                {
+                  text: "MONTO",
+                  fillColor: "#eaf2f5",
+                  border: [false, true, false, true],
+                  margin: [0, 5, 0, 5],
+                  textTransform: "uppercase",
+                },
               ],
               [
                 {
-                  text: observation,
+                  text: "observation",
                   border: [false, false, false, true],
                   margin: [0, 5, 0, 5],
                   alignment: "left",
                 },
                 {
                   border: [false, false, false, true],
-                  text: `$ ${expenseValue}`,
+                  text: "$ {expenseValue}",
                   fillColor: "#f5f5f5",
                   alignment: "right",
                   margin: [0, 5, 0, 5],
@@ -286,7 +198,7 @@ export default function ProofPaymentExpenses({ info }) {
                 },
                 {
                   border: [false, true, false, true],
-                  text: `$ ${expenseValue}`,
+                  text: "$ {expenseValue}",
                   alignment: "right",
                   fillColor: "#f5f5f5",
                   margin: [0, 5, 0, 5],
@@ -317,7 +229,7 @@ export default function ProofPaymentExpenses({ info }) {
                   margin: [0, 5, 0, 5],
                 },
                 {
-                  text: `$ ${expenseValue}`,
+                  text: "$ {expenseValue}",
                   bold: true,
                   fontSize: 20,
                   alignment: "right",
@@ -332,7 +244,7 @@ export default function ProofPaymentExpenses({ info }) {
         {
           text: [
             "La cantidad es: ",
-            { text: lettersExpenseValue, fontSize: 10, bold: false, italics: true },
+            { text: "lettersExpenseValue", fontSize: 10, bold: false, italics: true },
           ],
           fontSize: 10,
           bold: true,
@@ -364,29 +276,33 @@ export default function ProofPaymentExpenses({ info }) {
       },
     };
 
-    pdfMake.createPdf(proofExpenses).download();
-    // pdfMake.createPdf(tablaAmortizacionSimulador).open();
+    pdfMake.createPdf(expensisHistory).open();
   };
 
   return (
-    <MDButton
-      variant="text"
-      size="medium"
-      onClick={handleGeneratedPDF}
-      sx={{ background: "#5499C7", "&:hover": { background: "#8CB0C8" } }}
-    >
-      <PictureAsPdfIcon />
-    </MDButton>
+    <Grid container>
+      <MDButton
+        variant="text"
+        size="medium"
+        sx={{ background: "#5499C7", "&:hover": { background: "#8CB0C8" } }}
+      >
+        Excel
+      </MDButton>
+      <MDButton
+        variant="text"
+        size="medium"
+        onClick={handleGeneratedPDF}
+        sx={{ background: "#5499C7", "&:hover": { background: "#8CB0C8" } }}
+      >
+        PDF
+      </MDButton>
+      <MDButton
+        variant="text"
+        size="medium"
+        sx={{ background: "#5499C7", "&:hover": { background: "#8CB0C8" } }}
+      >
+        Descargar
+      </MDButton>
+    </Grid>
   );
 }
-
-ProofPaymentExpenses.propTypes = {
-  info: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      expenseDate: PropTypes.string.isRequired,
-      expenseValue: PropTypes.string.isRequired,
-      observation: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
