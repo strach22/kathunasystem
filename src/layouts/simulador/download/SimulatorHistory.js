dbbd7fa6a638c495a0b96ab31548efbfc5455b36
SebasDiUks <sebastian.lovato@outlook.com>
2022-05-27 11:40:15 -0500

    PDF Simulador

--- src/layouts/simulador/download/SimulatorHistory.js
+++ src/layouts/simulador/download/SimulatorHistory.js
@@ -1,12 +1,11 @@
-// import React, { useContext } from "react";
-import React from "react";
+import React, { useContext } from "react";
 import { Grid } from "@mui/material";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 import { makeStyles } from "@mui/styles";
 import { Workbook } from "react-excel-workbook";
-// import pdfMake from "pdfmake/build/pdfmake";
-// import pdfFonts from "pdfmake/build/vfs_fonts";
 import MDButton from "components/MDButton";
-// import ClientsContext from "context/Clients/ClientsContext";
+import ClientsContext from "context/Clients/ClientsContext";
 
 const useStyles = makeStyles({
   root: {
@@ -21,151 +20,144 @@ const useStyles = makeStyles({
 // eslint-disable-next-line react/prop-types
 export default function SimulatorHistory({ rows }) {
   const classes = useStyles();
+  const { controlInfo } = useContext(ClientsContext);
 
-  //   const { controlInfo } = useContext(ClientsContext);
+  function buildTableBody(data, columns) {
+    const body = [];
 
-  //   function buildTableBody(data, columns) {
-  //     const body = [];
+    const auxColumns = [
+      { text: "Código", alignment: "center", bold: true, color: "white", fontSize: 15 },
+      { text: "Interés Periodo", alignment: "center", bold: true, color: "white", fontSize: 15 },
+      { text: "Capital Amortizado", alignment: "center", bold: true, color: "white", fontSize: 15 },
+      { text: "Desgravamen", alignment: "center", bold: true, color: "white", fontSize: 15 },
+      { text: "Valor Cuota", alignment: "center", bold: true, color: "white", fontSize: 15 },
+      { text: "Saldo", alignment: "center", bold: true, color: "white", fontSize: 15 },
+    ];
 
-  //     const auxColumns = [
-  //       { text: "Periodo", alignment: "center", bold: true, color: "white", fontSize: 15 },
-  //       { text: "Interés Periodo", alignment: "center", bold: true, color: "white", fontSize: 15 },
-  //       { text: "Capital Amortizado", alignment: "center", bold: true, color: "white", fontSize: 15 },
-  //       { text: "Desgravamen", alignment: "center", bold: true, color: "white", fontSize: 15 },
-  //       { text: "Valor Cuota", alignment: "center", bold: true, color: "white", fontSize: 15 },
-  //       { text: "Saldo", alignment: "center", bold: true, color: "white", fontSize: 15 },
-  //     ];
+    body.push(auxColumns);
 
-  //     body.push(auxColumns);
+    data.forEach((row) => {
+      const dataRow = [];
 
-  //     data.forEach((row) => {
-  //       const dataRow = [];
+      columns.forEach((column) => {
+        dataRow.push({
+          text: row[column].toString(),
+          alignment: "left",
+          color: "black",
+          marginLeft: 25,
+        });
+      });
 
-  //       columns.forEach((column) => {
-  //         // if (column === "cuota")
-  //         //   dataRow.push({
-  //         //     text: row[column].toString(),
-  //         //     alignment: "left",
-  //         //     color: "black",
-  //         //     marginLeft: 25,
-  //         //   });
-  //         // else dataRow.push({ text: row[column].toString(), alignment: "center", color: "black" });
-  //         dataRow.push({ text: row[column].toString(), alignment: "center", color: "black" });
-  //       });
+      body.push(dataRow);
+    });
 
-  //       body.push(dataRow);
-  //     });
+    return body;
+  }
 
-  //     return body;
-  //   }
-
-  //   const simulatorPDF = {
-  //     pageMargins: [40, 40, 40, 80],
-  //     content: [
-  //       {
-  //         columns: [
-  //           [
-  //             {
-  //               text: controlInfo.nameBank,
-  //               color: "#333333",
-  //               width: "*",
-  //               fontSize: 18,
-  //               bold: true,
-  //               alignment: "center",
-  //               margin: [0, 0, 0, 3],
-  //             },
-  //             {
-  //               text: `''${controlInfo.nameSlogan}''`,
-  //               color: "#333333",
-  //               width: "*",
-  //               fontSize: 15,
-  //               bold: true,
-  //               alignment: "center",
-  //               margin: [0, 0, 0, 20],
-  //             },
-  //             {
-  //               text: controlInfo.nameLocation,
-  //               color: "#333333",
-  //               width: "*",
-  //               fontSize: 11,
-  //               bold: true,
-  //               alignment: "left",
-  //               margin: [10, 0, 0, 3],
-  //             },
-  //             "\n",
-  //             "\n\n",
-  //             {
-  //               text: "HISTORIAL DE GASTOS",
-  //               color: "#333333",
-  //               width: "*",
-  //               fontSize: 15,
-  //               bold: true,
-  //               alignment: "center",
-  //             },
-  //             {
-  //               text: "SIMULADOR TABLA DE AMORTIZACIÓN",
-  //               color: "#333333",
-  //               width: "*",
-  //               fontSize: 15,
-  //               bold: true,
-  //               alignment: "center",
-  //             },
-  //           ],
-  //         ],
-  //       },
-  //       {
-  //         text: "_________________________________________________________________",
-  //         style: "border",
-  //       },
-  //       {
-  //         layout: {
-  //           defaultBorder: false,
-  //           fillColor(rowIndex) {
-  //             if (rowIndex === 0) return "#088FCA";
-  //             return rowIndex % 2 === 0 ? "#85B1C4" : "#D3D3D3";
-  //           },
-  //           paddingLeft() {
-  //             return 10;
-  //           },
-  //           paddingRight() {
-  //             return 10;
-  //           },
-  //           paddingTop() {
-  //             return 5;
-  //           },
-  //           paddingBottom() {
-  //             return 5;
-  //           },
-  //         },
-  //         table: {
-  //           headerRows: 1,
-  //           widths: ["auto", "auto", "auto", "*"],
-  //           body: buildTableBody(rows, [
-  //             "cuota",
-  //             "interesPeriodo",
-  //             "capitalAmortizado",
-  //             "desgravamen",
-  //             "valorCuota",
-  //             "saldo",
-  //           ]),
-  //         },
-  //       },
-  //     ],
-
-  //     styles: {
-  //       border: {
-  //         fontSize: 15,
-  //         bold: true,
-  //         alignment: "center",
-  //         color: "black",
-  //         marginTop: 20,
-  //         marginBottom: 40,
-  //       },
-  //     },
-  //     defaultStyle: {
-  //       columnGap: 20,
-  //     },
-  //   };
+  const simulatorPDF = {
+    pageMargins: [40, 40, 40, 80],
+    content: [
+      {
+        columns: [
+          [
+            {
+              text: controlInfo.nameBank,
+              color: "#333333",
+              width: "*",
+              fontSize: 18,
+              bold: true,
+              alignment: "center",
+              margin: [0, 0, 0, 3],
+            },
+            {
+              text: `''${controlInfo.nameSlogan}''`,
+              color: "#333333",
+              width: "*",
+              fontSize: 15,
+              bold: true,
+              alignment: "center",
+              margin: [0, 0, 0, 3],
+            },
+            {
+              text: controlInfo.nameLocation,
+              color: "#333333",
+              width: "*",
+              fontSize: 12,
+              bold: true,
+              alignment: "center",
+              margin: [10, 0, 0, 25],
+            },
+            {
+              text: "TIPO DE TRANSACCIÓN",
+              color: "#333333",
+              width: "*",
+              fontSize: 15,
+              bold: true,
+              alignment: "center",
+            },
+            {
+              text: "SIMULADOR",
+              color: "red",
+              width: "*",
+              fontSize: 13,
+              bold: true,
+              alignment: "center",
+            },
+          ],
+        ],
+      },
+      {
+        text: "_________________________________________________________________",
+        style: "border",
+      },
+      {
+        layout: {
+          defaultBorder: false,
+          fillColor(rowIndex) {
+            if (rowIndex === 0) return "#088FCA";
+            return rowIndex % 2 === 0 ? "#85B1C4" : "#D3D3D3";
+          },
+          paddingLeft() {
+            return 10;
+          },
+          paddingRight() {
+            return 10;
+          },
+          paddingTop() {
+            return 5;
+          },
+          paddingBottom() {
+            return 5;
+          },
+        },
+        table: {
+          headerRows: 1,
+          widths: ["auto", "auto", "auto", "auto", "auto", "auto"],
+          body: buildTableBody(rows, [
+            "cuota",
+            "interesPeriodo",
+            "capitalAmortizado",
+            "desgravamen",
+            "valorCuota",
+            "saldo",
+          ]),
+        },
+      },
+    ],
+    styles: {
+      border: {
+        fontSize: 15,
+        bold: true,
+        alignment: "center",
+        color: "black",
+        marginTop: 20,
+        marginBottom: 40,
+      },
+    },
+    defaultStyle: {
+      columnGap: 20,
+    },
+  };
 
   const worksheets = [
     {
@@ -182,11 +174,12 @@ export default function SimulatorHistory({ rows }) {
     },
   ];
 
-  //   const handleGeneratedPDF = () => {
-  //     pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
-  //     pdfMake.createPdf(simulatorPDF).open();
-  //   };
+  const handleGeneratedPDF = () => {
+    if (!rows[0] === false) {
+      pdfMake.vfs = pdfFonts.pdfMake.vfs;
+      pdfMake.createPdf(simulatorPDF).open();
+    }
+  };
 
   return (
     <Grid container className={classes.root}>
@@ -213,7 +206,7 @@ export default function SimulatorHistory({ rows }) {
       <MDButton
         variant="text"
         size="medium"
-        // onClick={handleGeneratedPDF}
+        onClick={handleGeneratedPDF}
         sx={{ background: "#7B809A", "&:hover": { background: "#99A3A4" } }}
       >
         PDF
