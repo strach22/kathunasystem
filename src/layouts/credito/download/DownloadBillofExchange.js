6e63a96aaab9e7e082d58852379e964a240a3824
SebasDiUks <sebastian.lovato@outlook.com>
2022-05-31 18:20:13 -0500

    Letra de Cambio PDF

--- src/layouts/credito/download/DownloadBillofExchange.js
+++ src/layouts/credito/download/DownloadBillofExchange.js
@@ -15,8 +15,24 @@ export default function DownloadBillofExchange() {
   const i = clients.map((e) => e.id).indexOf(idC);
   const i2 = clients[i].credits.map((e) => e.id).indexOf(idF);
 
-  console.log(clients[i].credits[i2].initialDate);
-  console.log(clients[i].credits[i2].periods);
+  const auxInitialMonth = new Date(clients[i].credits[i2].initialDate);
+  const nowDate = new Date().toISOString().split("T")[0].replace("-", "/").replace("-", "/");
+
+  const auxLastMonth = auxInitialMonth;
+  const auxFirstMonth = auxInitialMonth;
+
+  auxLastMonth.setMonth(auxLastMonth.getMonth() + parseInt(clients[i].credits[i2].periods, 10));
+  auxFirstMonth.setMonth(auxFirstMonth.getMonth() + 1);
+
+  const lastMonth = auxLastMonth.toISOString().split("T")[0].replace("-", "/").replace("-", "/");
+  const firstMonth = auxFirstMonth.toISOString().split("T")[0].replace("-", "/").replace("-", "/");
+
+  const auxDate1 = clients[i].credits[i2].initialDate.split("/");
+  const auxDate2 = lastMonth.split("/");
+  const utcDate1 = Date.UTC(auxDate1[0], auxDate1[1] - 1, auxDate1[2]);
+  const utcDate2 = Date.UTC(auxDate2[0], auxDate2[1] - 1, auxDate2[2]);
+  const difference = utcDate2 - utcDate1;
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
 
   const changeTablePDF = {
     pageMargins: [40, 40, 40, 80],
@@ -42,55 +58,25 @@ export default function DownloadBillofExchange() {
                         "No: ",
                         {
                           text: zfill(parseInt(clients[i].credits[i2].id, 10), 3),
-                          style: "subtitle2",
+                          style: "text1",
                         },
                       ],
                       style: "title",
                       fontSize: 10,
                     },
                     {
+                      text: ["VENCE: ", { text: lastMonth, style: "subtitle2" }],
+                      style: "title",
+                      fontSize: 10,
+                    },
+                    {
                       text: [
-                        "VENCE: ",
-                        {
-                          text: zfill(parseInt(clients[i].credits[i2].id, 10), 3),
-                          style: "subtitle2",
-                        },
+                        "POR: ",
+                        { text: clients[i].credits[i2].loanValue, style: "subtitle2" },
                       ],
                       style: "title",
                       fontSize: 10,
                     },
-                    { text: "Fecha:", style: "Col1", width: 80 },
-                    { text: clients[i].credits[i2].initialDate, style: "Col2", width: 80 },
-                  ],
-                },
-                {
-                  columns: [
-                    { text: "RUC:", style: "Col1", width: 95 },
-                    { text: clients[i].identification, style: "Col2" },
-                    { text: "Teléfono:", style: "Col1", width: 80 },
-                    { text: clients[i].mobile, style: "Col2", width: 80 },
-                  ],
-                },
-                {
-                  columns: [
-                    { text: "Dirección:", style: "Col1", width: 95 },
-                    [{ text: clients[i].address, style: "Col2" }],
-                  ],
-                },
-                {
-                  columns: [
-                    { text: "Valor Préstamo:", style: "Col1", width: 95 },
-                    { text: clients[i].credits[i2].loanValue, style: "Col2" },
-                    { text: "Valor  Cuota:", style: "Col1", width: 80 },
-                    { text: clients[i].credits[i2].monthlyPayment, style: "Col2", width: 80 },
-                  ],
-                },
-                {
-                  columns: [
-                    { text: "Número de Cuotas:", style: "Col1", width: 95, marginBottom: 80 },
-                    { text: clients[i].credits[i2].periods, style: "Col2" },
-                    { text: "Total Interés:", style: "Col1", width: 80 },
-                    { text: clients[i].credits[i2].interest, style: "Col2", width: 80 },
                   ],
                 },
               ],
@@ -99,30 +85,56 @@ export default function DownloadBillofExchange() {
         ],
       },
       {
-        columns: [
-          [
-            { text: controlInfo.nameBank, style: "title", fontSize: 14 },
-            {
-              text: `''${controlInfo.nameSlogan}''`,
-              style: "title",
-              fontSize: 12,
-            },
-            { text: controlInfo.nameLocation, style: "title2" },
-            { text: "TIPO", style: "title", fontSize: 12 },
-            { text: "TABLA DE AMORTIZACIÓN", style: "subtitle" },
-            {
-              text: [
-                "CARPETA: ",
-                { text: zfill(parseInt(clients[i].credits[i2].id, 10), 3), style: "subtitle2" },
-              ],
-              style: "title",
-              fontSize: 10,
-            },
-          ],
+        text: [
+          "LUGAR Y FECHA: ",
+          { text: controlInfo.city, style: "subtitle2" },
+          { text: " ", style: "subtitle2" },
+          { text: nowDate, style: "subtitle2" },
         ],
+        style: "title",
+        fontSize: 10,
       },
-      "\n",
-      "\n\n",
+      {
+        text: [
+          "A ",
+          { text: days, style: "subtitle2" },
+          {
+            text: " días se servirá(n) Ud(s), pagar por esta LETRA DE CAMBIO ",
+            style: "subtitle2",
+          },
+        ],
+        style: "title",
+        fontSize: 10,
+      },
+      {
+        text: [
+          "A la orden de ",
+          { text: `${clients[i].firstName} ${clients[i].lastName}`, style: "subtitle2" },
+        ],
+        style: "title",
+        fontSize: 10,
+      },
+      {
+        text: [
+          "La cantidad de ",
+          { text: clients[i].credits[i2].loanValue, style: "subtitle2" },
+          { text: " dólares de los Estados Unidos de América", style: "subtitle2" },
+        ],
+        style: "title",
+        fontSize: 10,
+      },
+      {
+        text: [
+          "Con interés del __________% por ciento anual, desde ",
+          { text: firstMonth, style: "subtitle2" },
+        ],
+        style: "title",
+        fontSize: 10,
+      },
+      {
+        text: "Sin protesto. Exímese de presentación para aceptación y pago; así como de avisos por falta de estos hechos.",
+        style: "title2",
+      },
       {
         columns: [
           [
@@ -130,40 +142,35 @@ export default function DownloadBillofExchange() {
               stack: [
                 {
                   columns: [
-                    { text: "Cliente:", style: "Col1", width: 95 },
-                    { text: `${clients[i].firstName} ${clients[i].lastName}`, style: "Col2" },
-                    { text: "Fecha:", style: "Col1", width: 80 },
-                    { text: clients[i].credits[i2].initialDate, style: "Col2", width: 80 },
+                    { text: "A:", style: "title", fontSize: 12 },
+                    {
+                      text: `${clients[i].firstName} ${clients[i].lastName}`,
+                      style: "title",
+                      fontSize: 12,
+                    },
+                    { text: "Atentamente", style: "title", fontSize: 12 },
                   ],
                 },
                 {
                   columns: [
-                    { text: "RUC:", style: "Col1", width: 95 },
-                    { text: clients[i].identification, style: "Col2" },
-                    { text: "Teléfono:", style: "Col1", width: 80 },
-                    { text: clients[i].mobile, style: "Col2", width: 80 },
+                    { text: "Dirección:", style: "title", fontSize: 12 },
+                    {
+                      text: controlInfo.nameLocation,
+                      style: "title",
+                      fontSize: 12,
+                    },
+                    {},
                   ],
                 },
                 {
                   columns: [
-                    { text: "Dirección:", style: "Col1", width: 95 },
-                    [{ text: clients[i].address, style: "Col2" }],
-                  ],
-                },
-                {
-                  columns: [
-                    { text: "Valor Préstamo:", style: "Col1", width: 95 },
-                    { text: clients[i].credits[i2].loanValue, style: "Col2" },
-                    { text: "Valor  Cuota:", style: "Col1", width: 80 },
-                    { text: clients[i].credits[i2].monthlyPayment, style: "Col2", width: 80 },
-                  ],
-                },
-                {
-                  columns: [
-                    { text: "Número de Cuotas:", style: "Col1", width: 95, marginBottom: 80 },
-                    { text: clients[i].credits[i2].periods, style: "Col2" },
-                    { text: "Total Interés:", style: "Col1", width: 80 },
-                    { text: clients[i].credits[i2].interest, style: "Col2", width: 80 },
+                    { text: "Ciudad:", style: "title", fontSize: 12 },
+                    {
+                      text: controlInfo.city,
+                      style: "title",
+                      fontSize: 12,
+                    },
+                    { text: "________________", style: "title", fontSize: 12 },
                   ],
                 },
               ],
@@ -173,7 +180,7 @@ export default function DownloadBillofExchange() {
       },
     ],
     styles: {
-      title: {
+      text1: {
         color: "#333333",
         width: "*",
         bold: true,
