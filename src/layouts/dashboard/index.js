a438912a03d2d29d88fe05e8692245d89ec6b8af
Isaac <isaacandrespozo@gmail.com>
2022-05-31 10:51:09 -0500

    total ahorros

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 // @mui material components
@@ -15,10 +16,12 @@ import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 import InfoIcon from "examples/Cards/IconInfo";
 
 // Data
+import ClientsContext from "context/Clients/ClientsContext";
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
 import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
 
 function Dashboard() {
+  const { controlInfo } = useContext(ClientsContext);
   const { sales, tasks } = reportsLineChartData;
 
   return (
@@ -29,26 +32,36 @@ function Dashboard() {
           <Grid item xs={12} md={6} lg={3}>
             <MDBox>
               <Link to="/transaccionesAhorros">
-                <InfoIcon icon="iso" title="Transacciones" />
+                <InfoIcon icon="iso" title="Transacciones" count="$ 1000.00" />
               </Link>
             </MDBox>
           </Grid>
-
           <Grid item xs={12} md={6} lg={3}>
             <MDBox mb={1.5}>
               <Link to="/creditos">
-                <InfoIcon color="warning" icon="credit_card" title="Créditos" />
+                <InfoIcon color="warning" icon="credit_card" title="Créditos" count="$ 500.00" />
               </Link>
             </MDBox>
           </Grid>
           <Grid item xs={12} md={6} lg={3}>
             <MDBox mb={1.5}>
               <Link to="/gastos">
-                <InfoIcon color="dark" icon="paid" title="Gastos" />
+                <InfoIcon color="dark" icon="paid" title="Gastos" count="$ 500.00" />
               </Link>
             </MDBox>
           </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox>
+              <InfoIcon
+                color="success"
+                icon="attach_money"
+                title="Total"
+                count={`$ ${controlInfo.institutionSavings}`}
+              />
+            </MDBox>
+          </Grid>
         </Grid>
+
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
             <Grid item xs={12} md={6} lg={4}>
@@ -69,7 +82,7 @@ function Dashboard() {
                   title="daily sales"
                   description={
                     <>
-                      (<strong>+15%</strong>) increase in today sales.
+                      ( <strong> +15 % </strong>) increase in today sales.{" "}
                     </>
                   }
                   date="updated 4 min ago"
