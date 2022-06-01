c3e431202dbc4c9c5852d089cee164d79a4fbb0a
Isaac <isaacandrespozo@gmail.com>
2022-06-01 18:45:33 -0500

    graphs simulation

--- src/layouts/dashboard/data/reportsLineChartData.js
+++ src/layouts/dashboard/data/reportsLineChartData.js
@@ -1,18 +1,21 @@
 export default {
   ahorros: {
     labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"],
-    datasets: { label: "Ahorros", data: [0, 0, 0, 0, 500, 0, 0, 0, 0, 0, 0, 0] },
+    datasets: { label: "Ahorros", data: [0, 10000, 10050, 10100, 10100, 10100, 0, 0, 0, 0, 0, 0] },
   },
   creditos: {
     labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"],
-    datasets: { label: "Créditos", data: [0, 0, 0, 0, 500, 0, 0, 0, 0, 0, 0, 0] },
+    datasets: { label: "Créditos", data: [0, 70, 70, -930, -6050, -6050, 0, 0, 0, 0, 0, 0] },
   },
   gastos: {
     labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"],
-    datasets: { label: "Gastos", data: [0, 0, 0, 0, 500, 0, 0, 0, 0, 0, 0, 0] },
+    datasets: { label: "Gastos", data: [0, 130, 119.55, 608.41, 791.32, 0, 0, 0, 0, 0, 0, 0] },
   },
   total: {
     labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"],
-    datasets: { label: "Gastos", data: [0, 0, 0, 0, 500, 0, 0, 0, 0, 0, 0, 0] },
+    datasets: {
+      label: "Gastos",
+      data: [0, 9940, 10000.45, 9778.41, 4841.32, 40500, 0, 0, 0, 0, 0, 0],
+    },
   },
 };
