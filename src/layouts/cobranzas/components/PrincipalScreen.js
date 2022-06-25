b01f592d86c87245b0eb153ed9708beee11e1b9d
SebasDiUks <sebastian.lovato@outlook.com>
2022-06-25 11:48:07 -0500

    useMemo en tablas

--- src/layouts/cobranzas/components/PrincipalScreen.js
+++ src/layouts/cobranzas/components/PrincipalScreen.js
@@ -3,50 +3,42 @@ import { Card, Grid } from "@mui/material";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import DataTable from "examples/Tables/DataTable";
-
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
 import collectionsTable from "../tables/collectionsTable";
 
 export default function PrincipalScreen() {
   const { columns, rows } = collectionsTable();
 
   return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <MDBox pt={3}>
-        <Grid container spacing={6}>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h5" color="white">
-                  Registro de Pagos Vencidos
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <DataTable
-                  table={{ columns, rows }}
-                  isSorted={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                  entriesPerPage={false}
-                />
-              </MDBox>
-            </Card>
-          </Grid>
+    <MDBox pt={3}>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Card>
+            <MDBox
+              mx={2}
+              mt={-3}
+              py={3}
+              px={2}
+              variant="gradient"
+              bgColor="info"
+              borderRadius="lg"
+              coloredShadow="info"
+            >
+              <MDTypography variant="h5" color="white">
+                Registro de Pagos Vencidos
+              </MDTypography>
+            </MDBox>
+            <MDBox pt={3}>
+              <DataTable
+                table={{ columns, rows }}
+                isSorted={false}
+                showTotalEntries={false}
+                noEndBorder
+                entriesPerPage={false}
+              />
+            </MDBox>
+          </Card>
         </Grid>
-      </MDBox>
-      <Footer />
-    </DashboardLayout>
+      </Grid>
+    </MDBox>
   );
 }
