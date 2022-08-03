273989c8e3e515826d90d1503189ca3ecfd2ad32
SebasDiUks <sebastian.lovato@outlook.com>
2022-08-03 11:49:42 -0500

    La tabla de carpeta de créditos, sólo aparece cuando exista un crédito

--- src/layouts/credito/verCreditoForm/components/CreditGenerated.js
+++ src/layouts/credito/verCreditoForm/components/CreditGenerated.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import DataTable from "examples/Tables/DataTable";
-import { Link } from "react-router-dom";
-import MDButton from "components/MDButton";
 import CreditsInfo from "../../table/creditsInfo";
 
 export default function CreditGenerated() {
@@ -19,22 +17,6 @@ export default function CreditGenerated() {
           entriesPerPage={false}
           defaultEntries={5}
         />
-      </Grid>
-      <Grid item xs={8} sm={5} md={4} lg={3}>
-        <Link to="/creditos">
-          <MDButton
-            size="large"
-            variant="contained"
-            color="secondary"
-            sx={{
-              "&:hover": { background: "#99A3A4" },
-              width: "100%",
-              margin: "20px 0px 30px 30px",
-            }}
-          >
-            REGRESAR
-          </MDButton>
-        </Link>
       </Grid>
     </Grid>
   );
--- src/layouts/credito/verCreditoForm/components/PrincipalScreen.js
+++ src/layouts/credito/verCreditoForm/components/PrincipalScreen.js
@@ -1,11 +1,18 @@
-import React from "react";
-import MDBox from "components/MDBox";
+import React, { useContext, useMemo } from "react";
+import { Link, useParams } from "react-router-dom";
 import { Card, Grid } from "@mui/material";
+import MDButton from "components/MDButton";
+import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import InfoScreen from "layouts/credito/helpers/InfoScreen";
+import ClientsContext from "context/Clients/ClientsContext";
 import CreditGenerated from "./CreditGenerated";
 
 export default function PrincipalScreen() {
+  const id = useMemo(() => useParams().id, []);
+  const { clients } = useContext(ClientsContext);
+  const i = clients.map((e) => e.id).indexOf(id);
+
   return (
     <MDBox pt={6} pb={3} mx={4}>
       <Grid container spacing={6}>
@@ -28,29 +35,46 @@ export default function PrincipalScreen() {
             <MDBox pt={3}>
               <InfoScreen />
             </MDBox>
+            <Grid item xs={12} sm={7} md={4} lg={3} mx={5} mb={3}>
+              <Link to="/creditos">
+                <MDButton
+                  size="large"
+                  variant="contained"
+                  color="secondary"
+                  sx={{
+                    "&:hover": { background: "#99A3A4" },
+                    width: "100%",
+                  }}
+                >
+                  REGRESAR
+                </MDButton>
+              </Link>
+            </Grid>
           </Card>
         </Grid>
-        <Grid item xs={12}>
-          <Card>
-            <MDBox
-              mx={2}
-              mt={-3}
-              py={3}
-              px={2}
-              variant="gradient"
-              bgColor="info"
-              borderRadius="lg"
-              coloredShadow="info"
-            >
-              <MDTypography variant="h5" color="white">
-                Créditos Generados
-              </MDTypography>
-            </MDBox>
-            <MDBox pt={3}>
-              <CreditGenerated />
-            </MDBox>
-          </Card>
-        </Grid>
+        {clients[i].creditBalance > 0 && (
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h5" color="white">
+                  Créditos Generados
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <CreditGenerated />
+              </MDBox>
+            </Card>
+          </Grid>
+        )}
       </Grid>
     </MDBox>
   );
